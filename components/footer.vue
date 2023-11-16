f886f6da40e26bae0bab187b54f96752d08c495c
Alireza Arabi <alirezaaraby5@gmail.com>
2023-11-16 04:04:28 -0800

    dark mode/ nav/ footer added

--- components/footer.vue
+++ components/footer.vue
@@ -2,6 +2,28 @@
     <div class="bg-black rounded-t-lg">
         <h1 class="text-3xl font-bold underline text-center text-green-300">Summand</h1>
     </div>
+
+    <!-- <template> -->
+  <!--
+    Heads up! 👋
+  
+    Plugins:
+      - @tailwindcss/forms
+  -->
+  
+  <div>
+    <label for="UserEmail" class="block text-xs font-medium text-gray-700">
+      Email
+    </label>
+  
+    <input
+      type="email"
+      id="UserEmail"
+      placeholder="john@rhcp.com"
+      class="mt-1 w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+    />
+  </div>
+<!-- </template> -->
 </template>
 
 <script setup>
--- components/navbar.vue
+++ components/navbar.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container mt-4">
+    <div class="container z-20 mt-4">  <!--fixed-->
 
     
-    <nav class="bg-slate-500 rounded-2xl flex">
+    <nav class="backdrop-filter backdrop-blur-lg bg-opacity-30 rounded-2xl flex border border-[#192245]">
         <div class="flex flex-row gap-4 items-center">
             <div>
                 hi
