32d50dffbf28586150d12ba38a989393590693ee
Mazdak Pakaghideh <me@mazdak.dev>
2023-11-22 17:18:50 +0330

    update swiper

--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,9 @@
     <h1 class="text-3xl font-bold underline text-center text-green-300">Summand</h1>
 
     <h1 class="text-3xl font-bold text-center mt-12 mb-12">What people say</h1>
-    <Swiper :modules="[SwiperAutoplay, SwiperNavigation]" :slides-per-view="3" :loop="true" :autoplay="{ delay: 8000, disableOnInteraction: true }" :navigation="true"  :centeredSlides="true">
+    <Swiper :options="swiperOptions" :loop="true" :autoplay="{ delay: 8000, disableOnInteraction: true }" :navigation="true"  :centeredSlides="true" :slides-per-view="3">
         <SwiperSlide v-for="i in 10">
-            <div class="flex p-6">
-                <RecommandationCard class="recommandation-card"></RecommandationCard>
-            </div>
+          <RecommandationCard author="Ahmad">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut nemo veniam nostrum? Officiis voluptates optio provident tempora, delectus iste laborum modi enim! Impedit est sapiente ea quo optio, praesentium quae.</RecommandationCard>
         </SwiperSlide>
     </Swiper>
 
@@ -15,18 +13,33 @@
 </template>
 
 <script setup>
-
+const swiperOptions = {
+  modules:[SwiperAutoplay, SwiperNavigation],
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+  },
+};
 </script>
 
 <style>
 
 .swiper-slide {
-  @apply blur-[2px]
+  @apply blur-[2px] transition px-16; 
 }
 
 .swiper-slide-active {
-  @apply blur-0;
-  @apply  scale-110;
+  @apply blur-0 scale-110 transition;
+}
+
+.swiper-button-next, .swiper-button-prev {
+  @apply border-2 text-indigo-500 border-indigo-500 rounded-full w-10 h-10 flex items-center justify-center;
+}
+
+.swiper-button-next::after, .swiper-button-prev::after {
+  @apply text-xl; 
 }
 
 </style>
+
